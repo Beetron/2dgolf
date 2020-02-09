6c48ecc34a3fac45222f73eb6e66e2fca0b0173e
Liam <liam.s.oconnell@gmail.com>
2020-02-09 11:54:30 +1100

    Added control ring transparency

--- Ball.gd
+++ Ball.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 export var frictionSlowdown = 0.03
 export var power = 3
+onready var ring = $"Control Ring"
 
 var hitThreshold = 125 #Not sure how to get access to the edge of the ring directly
 
@@ -10,12 +11,15 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.pressed:
-		
-		#Execute a hit if within the threshold of the ring
-		if(self.global_position.distance_to(event.global_position) > hitThreshold):
-			var hit = self.global_position - event.global_position
-			apply_central_impulse(hit)
+	if (event is InputEventMouseButton and event.button_index == BUTTON_LEFT):	
+		if (event.pressed):
+			ring.visible = true
+		else:
+			#Execute a hit if within the threshold of the ring
+			if (self.global_position.distance_to(event.global_position) > hitThreshold):
+				var hit = self.global_position - event.global_position
+				apply_central_impulse(hit)
+				ring.visible = false
 	return	
 
 func _integrate_forces(state):
@@ -24,5 +28,8 @@ func _integrate_forces(state):
 	return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var distance = self.global_position.distance_to(get_viewport().get_mouse_position())
+	print(distance / hitThreshold)
+	ring.modulate.a = distance / hitThreshold
+	pass
